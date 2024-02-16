7c554f245c16eb4b839a78e54b1826e3073b3d85
Moebnooj <wnsqjadlehd@naver.com>
2024-02-16 18:38:38 +0900

    https://github.com/wnsqjadlehd/movie-web-service

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { nominalTypeHack } from "prop-types";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
+    }
+    
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+ return(
+  <div>
+    <h1>My ToDos ({toDos.length})</h1>
+    <form onSubmit={onSubmit}>
+  <input 
+  onChange={onChange}
+  value={toDo}
+  type="text"
+  placeholder="Write your to do..." 
+  />
+    <button>Add To Do</button>
+  </form>
+  <hr />
+  {toDos.map((item, index) => <li key={index}>{item}</li>)}
+ </div>
+ )
+}
+
+export default App;
